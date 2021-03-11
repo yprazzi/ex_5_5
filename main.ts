04b1c5dae066cdc5a57a3f57796dcd46f7f35d60
yprazzi <72910828+yprazzi@users.noreply.github.com>
2021-03-11 09:41:52 +0100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,32 +3,37 @@ basic.forever(function () {
     a = 0
     for (let index = 0; index < 5; index++) {
         led.plot(a, 4)
-        basic.pause(200)
+        basic.pause(75)
         a += 1
-        basic.pause(200)
+        basic.pause(75)
+        basic.clearScreen()
     }
     for (let index = 0; index < 6; index++) {
         led.plot(a, 3)
-        basic.pause(200)
+        basic.pause(75)
         a += -1
-        basic.pause(200)
+        basic.pause(75)
+        basic.clearScreen()
     }
     for (let index = 0; index < 6; index++) {
         led.plot(a, 2)
-        basic.pause(200)
+        basic.pause(75)
         a += 1
-        basic.pause(200)
+        basic.pause(75)
+        basic.clearScreen()
     }
     for (let index = 0; index < 6; index++) {
         led.plot(a, 1)
-        basic.pause(200)
+        basic.pause(75)
         a += -1
-        basic.pause(200)
+        basic.pause(75)
+        basic.clearScreen()
     }
     for (let index = 0; index < 6; index++) {
         led.plot(a, 0)
-        basic.pause(200)
+        basic.pause(75)
         a += 1
-        basic.pause(200)
+        basic.pause(75)
+        basic.clearScreen()
     }
 })
